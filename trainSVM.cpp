a5fbe2c26fc10fccd8fd22e4cfb15e8a1c2dc2f7
SteffiStuffel <steffi@freche-maedels.de>
2017-06-16 07:54:01 +0200

    Please enter the commit message for your changes. Lines starting modified gitignore and added some files

---
+++ trainSVM.cpp
@@ -0,0 +1,11 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/ml/ml.hpp>
+
+#include "trainSVM.h"
+
+using namespace std;
+using namespace cv;
+
+//define functions here
---
+++ trainSVM.h
@@ -0,0 +1,11 @@
+#ifndef TRAINSVM_H_INCLUDED
+#define TRAINSVM_H_INCLUDED
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+//declare functions here
+
+
+#endif
