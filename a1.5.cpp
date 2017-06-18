e2bec4d261a9a619d186216b929399fe524f6abd
FabioTe <fabiotempelmann@web.de>
2017-06-18 13:27:11 +0200

    change size with resize

--- a1.5.cpp
+++ a1.5.cpp
@@ -26,44 +26,46 @@ void testDownScale(int l) {
 
 	double scale = pow(2.0, 1.0/l);
 
-	int tmpl_width = 80;
-	int tmpl_height = 160;
+	int tmpl_width = 64;
+	int tmpl_height = 128;
 	double akt_width = img.cols;
 	double akt_height = img.rows;
-	Mat oldM = img.clone();
+	//Mat oldM = img.clone();
 	while (akt_width >= tmpl_width && akt_width >= tmpl_width) {
 		if (count % l == 0) {
 			double help = pow(2, count / l);
 			akt_width = img.cols / help;
 			akt_height = img.rows / help;
 			Mat m(floor(akt_height), floor(akt_width), CV_8UC3, Scalar(0, 0, 0));
-			for (int i = 0; i < floor(akt_height); i++) {
+			resize(img, m, Size(floor(akt_width), floor(akt_height)));
+			/*for (int i = 0; i < floor(akt_height); i++) {
 				for (int j = 0; j < floor(akt_width) - 0; j++) {
 					m.at<Vec3b>(i, j)[0] = img.at<Vec3b>(i * help, j * help)[0];
 					m.at<Vec3b>(i, j)[1] = img.at<Vec3b>(i * help, j * help)[1];
 					m.at<Vec3b>(i, j)[2] = img.at<Vec3b>(i * help, j * help)[2];
 				}
-			}
+			}*/
 			imshow("smaler", m);
 			waitKey();
 			destroyAllWindows();
-			oldM = m.clone();
+			//oldM = m.clone();
 		}
 		else {
 			akt_width = akt_width / scale;
 			akt_height = akt_height / scale;
 			Mat m(floor(akt_height), floor(akt_width), CV_8UC3, Scalar(0, 0, 0));
-			for (int i = 0; i < floor(akt_height); i++) {
+			resize(img, m, Size(floor(akt_width), floor(akt_height)));
+			/*for (int i = 0; i < floor(akt_height); i++) {
 				for (int j = 0; j < floor(akt_width) - 0; j++) {
 					m.at<Vec3b>(i, j)[0] = oldM.at<Vec3b>(i * scale, j * scale)[0];
 					m.at<Vec3b>(i, j)[1] = oldM.at<Vec3b>(i * scale, j * scale)[1];
 					m.at<Vec3b>(i, j)[2] = oldM.at<Vec3b>(i * scale, j * scale)[2];
 				}
-			}
+			}*/
 			imshow("smaler", m);
 			waitKey();
 			destroyAllWindows();
-			oldM = m.clone();
+			//oldM = m.clone();
 		}
 		count++;
 	}
